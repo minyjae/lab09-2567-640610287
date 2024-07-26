12110186c0fb6c625a62fe35a4cf73983520a03e
minyjae <jiradate_ora@cmu.ac.th>
2024-07-27 00:46:47 +0700

    Finish mung

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 import Task from "@components/Task";
@@ -9,35 +10,55 @@ import { useState } from "react";
 export default function Home() {
   // Define the interface of task-item object
   interface TaskItem {
-    // your code here
+    id: string;
+    title: string;
+    completed: boolean;
+    deleteTaskFunc: (taskId: string) => void; // callback function
+    toggleDoneTaskFunc: (taskId: string) => void; // callback function
   }
 
   // useState hook for an array of task-item objects
   const [tasks, setTasks] = useState<TaskItem[]>([]);
+  const [count, setCount] = useState(0);
 
   // Define the function with proper type
-  const addTask = (newTaskTitle) => {
-    const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
+  const addTask = (newTaskTitle : string) => {
+    const newTask = { id: nanoid(), 
+                      title: newTaskTitle, 
+                      completed: false,
+                      deleteTaskFunc : deleteTask, 
+                      toggleDoneTaskFunc : toggleDoneTask};
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
   };
 
   // Define the function with proper type
-  const deleteTask = (taskId) => {
+  const deleteTask = (taskId : string) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    const count = tasks.length;
+    setCount(count-1);
   };
 
   // Define the function with proper type
-  const toggleDoneTask = (taskId) => {
+  const toggleDoneTask = (taskId : string) => {
     //structuredClone will copy an array or an object "deeply"
     //So objects within an object will be copied too
-    const newTasks = structuredClone(tasks);
+    // console.log(tasks);
+    // const newTasks = structuredClone(tasks);
     //search for a task based on condition
-    const task = newTasks.find((x) => x.id === taskId);
-    task.completed = !task.completed;
-    setTasks(newTasks);
+    const newTask = tasks.find((x) => x.id === taskId);
+    if(newTask !== undefined){
+    newTask.completed = !newTask.completed;
+    setTasks(tasks);
+    addCountDone();
+    }
   };
+
+  const addCountDone = () => {
+    const countUpdate = tasks.filter(task => task.completed).length;
+    setCount(countUpdate);
+  }
 
   return (
     // Main container
@@ -48,7 +69,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({tasks.length}) Done ({count})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -67,7 +88,7 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2024" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer year="2024" fullName="Jiradate Oratai" studentId="640610287" />
     </div>
   );
 }
